c214baac4470461d7e82b8cdd466a0413036c43c
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2023-07-27 17:45:20 +0900

    feat:책_카테고리_설정

--- BackEnd/BAOBAB/Category/models.py
+++ BackEnd/BAOBAB/Category/models.py
@@ -4,3 +4,6 @@ class Category(models.Model):
     category_name = models.CharField(max_length=255, primary_key=True, unique=True)
     mainCategory_name = models.OneToOneField('self', on_delete=models.CASCADE, unique=True, null=True, blank=True)
     is_main = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.category_name
